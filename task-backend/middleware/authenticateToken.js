f754c60567ec3927439720b940d201982c550091
rajan <rajansuhagiya1234g@gmail.com>
2024-03-14 13:33:14 +0530

    change

--- task-backend/middleware/authenticateToken.js
+++ task-backend/middleware/authenticateToken.js
@@ -9,7 +9,7 @@ const authenticateToken = (req,res,next) => {
         return res.sendStatus(401);
     }
 
-    jwt.verify(token, process.env.JWT_SECRET, (err,user)=>{
+    jwt.verify(token, process.env.JWT_SECRET, async (err,user)=>{
         if(err){
             return res.sendStatus(403);
         }
